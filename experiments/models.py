8207cbda8bc33ebf6c2931f23be31b5842525b67
cesslab <anwarruff@gmail.com>
2018-03-24 05:10:44 -0400

    created experiment models

--- experiments/models.py
+++ experiments/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
+from model_utils import TimeStampedModel
+
+User = get_user_model()
+
+
+class Location(TimeStampedModel):
+    address = models.CharField(max_length=255, blank=False)
+    slug = models.CharField(max_length=255, unique=True)
+
+
+class UniversityDepartment(TimeStampedModel):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class ResearcherProfile(TimeStampedModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    department = models.ForeignKey(
+        UniversityDepartment, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+
+class Experiment(TimeStampedModel):
+    researchers = models.ManyToManyField(User)
+    name = models.CharField(max_length=255)
+    human_subject_id = models.CharField(max_length=255, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Session(TimeStampedModel):
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    experiment = models.ForeignKey(Experiment, on_delete=models.CASCADE)
+    session_number = models.PositiveIntegerField(default=0)
+    start_time = models.DateTimeField(blank=False)
+    end_time = models.DateTimeField(blank=False)
+
+    def __str__(self):
+        return "Session {} ({}-{})".format(
+            self.start_time.strftime("%m%d%Y%H%M"))
+
 # Create your models here.
