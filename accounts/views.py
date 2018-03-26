8f210a5dbbc55288b650ed90775d8055ac5fb927
cesslab <anwarruff@gmail.com>
2018-03-26 09:01:00 -0400

    replace login func w. CBV

--- accounts/views.py
+++ accounts/views.py
@@ -1,20 +1,33 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
 
 from .forms import LoginForm
 
 
-def login_page(request):
-    form = LoginForm(request.POST)
-    context = {'form': form}
-    if form.is_valid():
-        email = form.cleaned_data.get("email")
-        password = form.cleaned_data.get("password")
-        user = authenticate(request, email=email, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('/login')
-        else:
-            print("Error")
+class LoginView(LoginView):
+    authentication_form = LoginForm
+    template_name = 'accounts/login.html'
 
-    return render(request, "accounts/login.html", context)
+    def get_form_kwargs(self):
+        kwargs = super(LoginView, self).get_form_kwargs()
+        kwargs.update({'request': self.request})
+        return kwargs
+
+    def form_valid(self, form):
+        return redirect(reverse_lazy('home'))
+
+# def login_page(request):
+#     form = LoginForm(request.POST)
+#     context = {'form': form}
+#     if form.is_valid():
+#         email = form.cleaned_data.get("email")
+#         password = form.cleaned_data.get("password")
+#         user = authenticate(request, email=email, password=password)
+#         if user is not None:
+#             login(request, user)
+#             return redirect('/login')
+#         else:
+#             print("Error")
+#
+#     return render(request, "accounts/login.html", context)
